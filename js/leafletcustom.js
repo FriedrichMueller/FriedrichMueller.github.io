92870b285ca7c3b01236731c225530fe6cbddab3
FriedrichMueller <mue-friedrich@web.de>
2016-11-25 22:15:15 +0100

    Revert "STM4"
    This reverts commit 8cc7e671a9f518fa3f13b5f19ef8898942baba7b.

--- js/leafletcustom.js
+++ js/leafletcustom.js
@@ -1,8 +1,7 @@
 			// create the map object and set the cooridnates of the initial view:
 			var layer = new L.StamenTileLayer("toner");
 			var map = new L.Map("map", {
-				//center: new L.LatLng(51.532689, 9.935121),
-				center: new L.LatLng(0, 0),
+				center: new L.LatLng(51.532689, 9.935121),
 				zoom: 14
 			});
 			map.addLayer(layer);
