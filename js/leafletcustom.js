a86a01523ab93b2b24b225391280596711a2d19d
Friedrich Müller <FriedrichMueller@users.noreply.github.com>
2019-03-26 22:09:10 +0100

    --

--- js/leafletcustom.js
+++ js/leafletcustom.js
@@ -1,13 +1,13 @@
 			// create the map object and set the cooridnates of the initial view:
 			var layer = new L.StamenTileLayer("toner");
 			var map = new L.Map("map", {
-				center: new L.LatLng(51.532689, 9.935121),
+				center: new L.LatLng(47.644952,9.396864),
 				zoom: 14
 			});
 			map.addLayer(layer);
 
-			
-			
+
+
 			var imageContainerMargin = 70;  // Margin + padding
 
 // This watches for the scrollable container
